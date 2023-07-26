b653d8783e5d4fc57152cfedf1b965bc521bfac6
Aman Jangra <aman.12007267@lpu.in>
2023-07-26 18:51:28 +0530

    ok9 test

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 mongoose.connect(process.env.DB_URL);
 
-const routes = require('./api/product');
+const routes = require('./api/Index');
 
 app.use('/api', routes);
 
