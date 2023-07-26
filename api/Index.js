42282b070a03c92a3f5e027d0d218763fe97c337
Aman Jangra <aman.12007267@lpu.in>
2023-07-27 00:08:27 +0530

    Good Night

--- api/Index.js
+++ api/Index.js
@@ -1,16 +1,38 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
-const cors =require('cors');
-const { default: mongoose } = require('mongoose');
-const User =require('./models/User');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const User = require('./models/User');
 const Post = require('./models/Post');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const  cookieParser =  require('cookie-parser');
+const cookieParser = require('cookie-parser');
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const multer = require('multer');
 const cloudinary = require('cloudinary').v2;
+require('dotenv').config();
+const fs = require('fs');
 
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const salt = bcrypt.genSaltSync(10);
+const secret = 'kjfbejajbgfiuwu392rjdvjsdgwrugvisjdnvlhg9r';
+
+const corsOptions = {
+  credentials: true,
+  origin: ['http://localhost:3000']
+};
+
+router.use(cors(corsOptions));
+mongoose.connect(process.env.DB_URL);
+router.use(express.json());
+router.use(cookieParser());
+
+// Cloudinary configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
+
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
@@ -19,178 +41,117 @@ const storage = new CloudinaryStorage({
   },
 });
 
-const uploadMiddleware = multer({ storage: storage });
-const fs = require('fs');
-require('dotenv').config();
 
+router.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const UserDoc = await User.create({
+      username,
+      password: bcrypt.hashSync(password, salt)
+    });
+    res.json(UserDoc);
+  } catch (e) {
+    res.status(400).json(e);
+  }
+});
 
-const salt = bcrypt.genSaltSync(10);
-const secret  = 'kjfbejajbgfiuwu392rjdvjsdgwrugvisjdnvlhg9r';   
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  const UserDoc = await User.findOne({ username });
+  const passok = bcrypt.compareSync(password, UserDoc.password);
+  if (passok) {
+    jwt.sign({ username, id: UserDoc._id }, secret, {}, (err, token) => {
+      if (err) throw err;
+      res.cookie('token', token).json({
+        id: UserDoc._id,
+        username
+      });
+    });
+  } else {
+    res.status(400).json('wrong credentials');
+  }
+});
 
+router.get('/profile', (req, res) => {
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, (err, info) => {
+    if (err) throw err;
+    res.json(info);
+  });
+});
 
+router.post('/logout', (req, res) => {
+  res.cookie('token', '').json('ok');
+});
 
-
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET,
+router.post('/post', multer({ storage: storage }).single('file'), async (req, res) => {
+  const { title, summary, content } = req.body;
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, async (err, info) => {
+    if (err) throw err;
+    const postDoc = await Post.create({
+      title,
+      summary,
+      content,
+      cover: req.file ? req.file.path : null,
+      author: info.id,
+    });
+    res.json(postDoc);
+  });
 });
 
 
-const corsOptions = {
-        credentials:true,
-        origin: ['http://localhost:3000']
-};
+router.put('/post', multer({ storage: storage }).single('file'), async (req, res) => {
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, async (err, info) => {
+    if (err) throw err;
+    const { id, title, summary, content } = req.body;
+    const postDoc = await Post.findById(id);
+    if (!postDoc) {
+      return res.status(404).json('Post not found');
+    }
+    const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+    if (!isAuthor) {
+      return res.status(400).json('You are not the author');
+    }
 
-router.use(cors(corsOptions)); 
-mongoose.connect(process.env.DB_URL)
-router.use(express.json());
-router.use(cookieParser());
-// app.use('/uploads',express.static(__dirname + '/uploads'));
+    // Update the post with the new data and cover image URL if available
+    postDoc.title = title;
+    postDoc.summary = summary;
+    postDoc.content = content;
+    if (req.file) {
+      // Upload the new image to Cloudinary
+      const result = await cloudinary.uploader.upload(req.file.path);
+      postDoc.cover = result.secure_url;
 
+      // Remove the temporary file after upload (Not necessary for Cloudinary)
+      // fs.unlinkSync(req.file.path);
+    }
 
-//for Register
-router.post('/register', async(req,res)=>{
-        const {username,password}=req.body;
-       try{
-        const UserDoc = await User.create({
-                username,
-                password:bcrypt.hashSync(password,salt)})
-        res.json(UserDoc);
-       }catch(e){
-        res.status(400).json(e);
-       }
-}); 
-
-
-
-//for login
-router.post('/login', async(req,res)=>{
-        const {username,password}=req.body;
-        const UserDoc = await User.findOne({ username});
-        const passok = bcrypt.compareSync(password,UserDoc.password);
-        // res.json(passok);
-        if(passok){
-                jwt.sign({username,id:UserDoc._id},secret, {}, (err,token)=>{
-                        if(err) throw err;
-                        res.cookie('token',token).json({
-                                id:UserDoc._id,
-                                username,
-                        });
-                });
-        }else{
-                res.status(400).json('wrong credentials');
-        }
-}); 
-router.get('/profile',(req,res) =>{
-        const {token} = req.cookies;
-        jwt.verify(token,secret,{}, (err,info)=>{
-                if(err) throw err;
-                res.json(info);
-        });
-        
-});
-
-router.post('/logout',(req,res) => {
-        res.cookie('token','').json('ok');
+    await postDoc.save();
+    res.json(postDoc);
+  });
 });
 
 
-router.post('/post', uploadMiddleware.single('file'), async (req, res) => {
-        const { originalname, path } = req.file;
-        const parts = originalname.split('.');
-        const ext = parts[parts.length - 1];
-        const newPath = path + '.' + ext;
-        fs.renameSync(path, newPath);
-      
-        const { token } = req.cookies;
-        jwt.verify(token, secret, {}, async (err, info) => {
-          if (err) throw err;
-          const { title, summary, content } = req.body;
-      
-          try {
-            // Upload the image to Cloudinary
-            const cloudinaryResponse = await cloudinary.uploader.upload(newPath, {
-              folder: 'blog_images', // Specify the folder in Cloudinary where you want to save the image
-            });
-      
-            const PostDoc = await Post.create({
-              title,
-              summary,
-              content,
-              cover: cloudinaryResponse.secure_url, // Save the secure URL of the uploaded image
-              author: info.id,
-            });
-      
-            res.json(PostDoc.cover);
-          } catch (error) {
-            console.error(error);
-            res.status(500).json({ error: 'Failed to upload the image to Cloudinary' });
-          }
-        });
-      });
+router.get('/post', async (req, res) => {
+  res.json(
+    await Post.find()
+      .populate('author', ['username'])
+      .sort({ createdAt: -1 })
+      .limit(20)
+  );
+});
 
-router.put('/post', uploadMiddleware.single('file'), async (req, res) => {
-        let newPath = null;
-        if (req.file) {
-          const { originalname, path } = req.file;
-          const parts = originalname.split('.');
-          const ext = parts[parts.length - 1];
-          newPath = path + '.' + ext;
-          fs.renameSync(path, newPath);
-        }
-      
-        const { token } = req.cookies;
-        jwt.verify(token, secret, {}, async (err, info) => {
-          if (err) throw err;
-          const { id, title, summary, content } = req.body;
-          const postDoc = await Post.findById(id);
-          const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
-          if (!isAuthor) {
-            return res.status(400).json('you are not the author');
-          }
-      
-          try {
-            let coverUrl = newPath ? postDoc.cover : postDoc.cover;
-            if (newPath) {
-              // Upload the updated image to Cloudinary
-              const cloudinaryResponse = await cloudinary.uploader.upload(newPath, {
-                folder: 'blog_images',
-              });
-              coverUrl = cloudinaryResponse.secure_url;
-            }
-      
-            await postDoc.updateOne({
-              title,
-              summary,
-              content,
-              cover: coverUrl, // Update the secure URL of the image
-            });
-      
-            res.json(postDoc);
-          } catch (error) {
-            console.error(error);
-            res.status(500).json({ error: 'Failed to upload the image to Cloudinary' });
-          }
-        });
-      });
-      
+router.get('/post/:id', async (req, res) => {
+  const { id } = req.params;
+  const postDoc = await Post.findById(id).populate('author', ['username']);
+  if (!postDoc) {
+    return res.status(404).json('Post not found');
+  }
+  res.json(postDoc);
+});
 
 
-router.get('/post', async (req,res) => {
-        res.json(
-          await Post.find()
-            .populate('author', ['username'])
-            .sort({createdAt: -1})
-            .limit(20)
-        );
-      });
-
-
-router.get('/post/:id', async (req,res)=>{
-        const {id,author} = req.params;
-        const  postDoc = await  Post.findById(id).populate('author',['username']);
-        res.json(postDoc);
-})
 module.exports = router;
 //mongodb+srv://blogit:XbxGk2uinecT3NUI@blogcluster0.idzmzcj.mongodb.net/?retryWrites=true&w=majority
